7e8ecdce344a6e259ad44a4337a354cd7d48ff9d
Mark Jopriz O. Cabelinga <markjopriz@gmail.com>
2015-07-27 22:11:33 +0800

    added for IE drivers

--- Automation/src/testClass.java
+++ Automation/src/testClass.java
@@ -3,11 +3,13 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class testClass {
 	private WebDriver driver;
 	@Before
 	public void setUp() throws Exception{
+		/*switch for chrome browser
 		System.setProperty("webdriver.chrome.driver", "C://Selenium//chromedriver_win32//chromedriver.exe");
 		// To remove message "You are using an unsupported command-line flag: --ignore-certificate-errors.
 	    // Stability and security will suffer."
@@ -17,23 +19,32 @@ public class testClass {
 	    options.addArguments("test-type");
 	    capabilities.setCapability("chrome.binary","C://Program Files (x86)//Google//Chrome//Application//");
 	    capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-
 		driver = new ChromeDriver(capabilities);
+		*/
+		
+		//for IE
+		System.setProperty("webdriver.ie.driver", "C://Selenium//IEDriverServer_Win32_2.44.0//IEDriverServer.exe");
+		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+		//Should be changed later on, see http://jimevansmusic.blogspot.com/2012/08/youre-doing-it-wrong-protected-mode-and.html
+		capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		driver = new InternetExplorerDriver(capabilities);
 	}
-	
 	@Test
 	public void mainTestClass() {
+		//TC - SP001
 		driver.get("http://www.google.com.ph");
-		driver.findElement(By.name("q")).sendKeys("selenium");
-		driver.findElement(By.name("btnG")).click();
-		System.out.println("Test 1");
-//		driver.get("http://www.wikipedia.org/");
-//		Assert.assertEquals("Wikipedia", driver.getTitle());
+		driver.findElement(By.id("gb_70")).click();
+		
+		//TC - SP002
+		driver.findElement(By.id("Email")).sendKeys("testEmail");
+		driver.findElement(By.id("next")).click();
+		
+		//TC - SP003
+		
 	}
 	
-	/*@After
+	@After
 	public void tearDown() throws Exception{
-		driver.quit();
-	}*/
-
+//		driver.quit();
+	}
 }
