e584638a32fb4bf1c7d3a2082cd2d79dd6dbd0ae
Mark Jopriz O. Cabelinga <markjopriz@gmail.com>
2015-07-28 19:29:46 +0800

    test

--- Automation/src/testClass.java
+++ Automation/src/testClass.java
@@ -1,4 +1,9 @@
-import org.junit.*;
+//import org.junit.*;
+//import org.testng.*;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,20 +12,19 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class testClass {
 	private WebDriver driver;
-	@Before
+	@BeforeClass
 	public void setUp() throws Exception{
-		/*switch for chrome browser
-		System.setProperty("webdriver.chrome.driver", "C://Selenium//chromedriver_win32//chromedriver.exe");
-		// To remove message "You are using an unsupported command-line flag: --ignore-certificate-errors.
-	    // Stability and security will suffer."
-	    // Add an argument 'test-type'
-	    DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-	    ChromeOptions options = new ChromeOptions();
-	    options.addArguments("test-type");
-	    capabilities.setCapability("chrome.binary","C://Program Files (x86)//Google//Chrome//Application//");
-	    capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-		driver = new ChromeDriver(capabilities);
-		*/
+		//for Chrome
+//		System.setProperty("webdriver.chrome.driver", "C://Selenium//chromedriver_win32//chromedriver.exe");
+//		// To remove message "You are using an unsupported command-line flag: --ignore-certificate-errors.
+//	    // Stability and security will suffer."
+//	    // Add an argument 'test-type'
+//	    DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//	    ChromeOptions options = new ChromeOptions();
+//	    options.addArguments("test-type");
+//	    capabilities.setCapability("chrome.binary","C://Program Files (x86)//Google//Chrome//Application//");
+//	    capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+//		driver = new ChromeDriver(capabilities);
 		
 		//for IE
 		System.setProperty("webdriver.ie.driver", "C://Selenium//IEDriverServer_Win32_2.44.0//IEDriverServer.exe");
@@ -31,21 +35,25 @@ public class testClass {
 	}
 	@Test
 	public void mainTestClass() {
-		//TC - SP001
+		//TC - SP001 (Browser and webpage is successfully opened)
 		driver.get("http://www.google.com.ph");
+		Assert.assertEquals("Google", 
+				driver.getTitle());
+		System.out.println("TC SP001 done.");
+		
+		//TC - SP002 (UserID exists)
 		driver.findElement(By.id("gb_70")).click();
-//		Assert.
-//		Assert.assertEquals("Wikipedia", driver.getTitle());
+		Assert.assertEquals("Sign in - Google Accounts", 
+				driver.getTitle());
+		System.out.println("TC SP002 done.");
 		
-		//TC - SP002
-		driver.findElement(By.id("Email")).sendKeys("testEmail");
-		driver.findElement(By.id("next")).click();
-		
-		//TC - SP003
+		//TC - SP003 (Password exists)
+//		driver.findElement(By.id("Email")).sendKeys("testEmail");
+//		driver.findElement(By.id("next")).click();
 		
 	}
 	
-	@After
+	@AfterClass
 	public void tearDown() throws Exception{
 //		driver.quit();
 	}
